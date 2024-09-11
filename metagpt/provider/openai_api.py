2b45ce3a241017cd9d70f8460345c7ac7e48b320
Justin-ZL <527857162@qq.com>
2024-09-11 16:26:42 +0800

    Fix issue #1177: Add check for None in chunk.usage before passing to CompletionUsage

--- metagpt/provider/openai_api.py
+++ metagpt/provider/openai_api.py
@@ -105,7 +105,8 @@ class OpenAILLM(BaseLLM):
                     if isinstance(chunk.usage, CompletionUsage):
                         usage = chunk.usage
                     else:
-                        usage = CompletionUsage(**chunk.usage)
+                        if chunk.usage is not None:
+                            usage = CompletionUsage(**chunk.usage)
                 elif hasattr(chunk.choices[0], "usage"):
                     # The usage of some services is an attribute of chunk.choices[0], such as Moonshot
                     usage = CompletionUsage(**chunk.choices[0].usage)
